71101946b6763efe2b260b08a5915799b71c9ebe
Drew <drewclementsdesign@gmail.com>
2022-03-01 00:18:14 -0500

    last thing for the night

--- components/OurServices.vue
+++ components/OurServices.vue
@@ -1,10 +1,10 @@
 <template>
   <section class="base-wrapper my-24 lg:my-48">
     <div class="grid lg:grid-cols-2 gap-12 items-center">
-      <div class="lg:w-3/4">
+      <div class="lg:border-r border-gray-100">
         <p class="text-xs uppercase mb-4">{{ blok.kicker }}</p>
         <FancyHeading class="text-3xl lg:text-4xl lg:w-4/5 mb-8">{{ blok.heading }}</FancyHeading>
-        <BaseText class="opacity-75">{{ blok.text }}</BaseText>
+        <BaseText class="opacity-75 lg:w-3/4">{{ blok.text }}</BaseText>
       </div>
 
       <div class="grid lg:grid-cols-2 gap-6">
@@ -13,6 +13,7 @@
           <div class="text-sm opacity-75">
             <p class="mb-6">{{ service.text }}</p>
 
+            <hr class="mb-6 w-1/2" />
             <ul>
               <li class="mb-1" v-for="item in service.items" :key="item.id">{{ item.label }}</li>
             </ul>
