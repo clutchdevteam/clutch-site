a101f958cc27e6ee4acac92baeb2bfdf589dd4c6
Drew <drewclementsdesign@gmail.com>
2022-03-08 15:18:02 -0500

    some animations

--- components/TwoColumnVideo.vue
+++ components/TwoColumnVideo.vue
@@ -1,18 +1,28 @@
 <template>
   <section class="my-28 md:my-64 pb-12 md:pb-32">
     <div class="md:grid grid-cols-2 2xl:grid-cols-3 gap-12 items-center">
-      <div class="relative order-last 2xl:col-span-2 mb-12 md:mb-0 pl-8 md:pl-0">
+      <div
+        ref="videoRef"
+        class="relative order-last 2xl:col-span-2 mb-12 md:mb-0 pl-8 md:pl-0"
+      >
         <img class="block z-10" :src="blok.video.filename" :alt="blok.alt" />
 
         <div
           class="absolute bg-primary top-0 transform -translate-x-12 translate-y-12 md:-translate-x-24 md:translate-y-24"
           style="z-index: -999"
         >
-          <img :src="blok.video.filename" :alt="blok.alt" style="mix-blend-mode: multiply" />
+          <img
+            :src="blok.video.filename"
+            :alt="blok.alt"
+            style="mix-blend-mode: multiply"
+          />
         </div>
       </div>
 
-      <div class="order-first 2xl:col-span-1 mt-24 md:mt-0 p-4 md:p-0">
+      <div
+        ref="contentRef"
+        class="order-first 2xl:col-span-1 mt-24 md:mt-0 p-4 md:p-0"
+      >
         <div class="mb-4">
           <FancyHeading class="text-3xl lg:text-4xl mb-8">
             <span v-html="blok.title" />
@@ -28,7 +38,12 @@
 </template>
 
 <script>
-import FancyHeading from './FancyHeading.vue';
+import FancyHeading from "./FancyHeading.vue";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
 export default {
   components: {
     FancyHeading,
@@ -38,6 +53,46 @@ export default {
       type: Object,
       required: true,
     },
+  },
+  computed: {
+    videoRef() {
+      return this.$refs.videoRef;
+    },
+    contentRef() {
+      return this.$refs.contentRef;
+    },
+  },
+  mounted() {
+    const query = "(prefers-reduced-motion: no-preference)";
+    const mediaQueryList = window.matchMedia(query);
+    const prefersReducedMotion = !mediaQueryList.matches;
+
+    if (prefersReducedMotion) {
+      null;
+    } else {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: this.videoRef,
+          start: "top bottom",
+          end: "center center",
+        },
+      });
+
+      tl.from(this.videoRef, { opacity: 0, scale: 0, ease: "back" }).to(
+        this.videoRef,
+        {
+          opacity: 1,
+          scale: 1,
+        }
+      );
+      tl.from(this.contentRef, { y: -100, opacity: 0, duration: 0.25 }, 0.5).to(
+        this.contentRef,
+        {
+          y: 0,
+          opacity: 1,
+        }
+      );
+    }
   },
 };
 </script>
