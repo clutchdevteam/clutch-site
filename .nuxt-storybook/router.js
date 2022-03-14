64b23f1224e98f63e6cb58130cde688fc8100ab4
Drew <drewclementsdesign@gmail.com>
2022-03-14 16:41:20 -0400

    subscription form

--- .nuxt-storybook/router.js
+++ .nuxt-storybook/router.js
@@ -5,6 +5,7 @@ import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
 const _ad4b9162 = () => interopDefault(import('../pages/contact-us.vue' /* webpackChunkName: "pages/contact-us" */))
+const _5e0dbe96 = () => interopDefault(import('../pages/subscribe.vue' /* webpackChunkName: "pages/subscribe" */))
 const _0f65432b = () => interopDefault(import('../pages/_.vue' /* webpackChunkName: "pages/_" */))
 
 const emptyFn = () => {}
@@ -23,6 +24,11 @@ export const routerOptions = {
     component: _ad4b9162,
     pathToRegexpOptions: {"strict":true},
     name: "contact-us"
+  }, {
+    path: "/subscribe",
+    component: _5e0dbe96,
+    pathToRegexpOptions: {"strict":true},
+    name: "subscribe"
   }, {
     path: "/*",
     component: _0f65432b,
